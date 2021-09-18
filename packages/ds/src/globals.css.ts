cf507c5aae88fb2005d6d82eb28d331bb6a63669
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-09-18 19:40:48 +0200

    Refactor theme

---
+++ packages/ds/src/globals.css.ts
@@ -0,0 +1,83 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+import colors from "./colors";
+import { darkMode } from "./theme.css";
+
+globalStyle(`.${darkMode}`, {
+  background: colors.coolGray[900],
+  color: colors.coolGray[400],
+});
+
+globalStyle("html", {
+  fontFeatureSettings: "'ss01' on,'ss02' on,'cv01' on,'cv03' on",
+  background: colors.white,
+  color: colors.coolGray[600],
+});
+
+globalStyle("::selection", {
+  background: colors.blue[500],
+  color: colors.black,
+});
+
+globalStyle(`${darkMode} ::placeholder`, {
+  color: colors.coolGray[500],
+});
+
+globalStyle("::placeholder", {
+  color: colors.coolGray[400],
+  opacity: 1,
+});
+
+// NOTE: -webkit-* for some reason doesn't work with just .dark, requires element.dark.
+globalStyle(`html.${darkMode} *::-webkit-scrollbar`, {
+  background: colors.coolGray[900],
+});
+
+globalStyle("*::-webkit-scrollbar", {
+  background: colors.white,
+  width: 12,
+  height: 12,
+});
+
+globalStyle(`html.${darkMode} *::-webkit-scrollbar-corner`, {
+  background: colors.coolGray[900],
+});
+
+globalStyle("*::-webkit-scrollbar-corner", {
+  background: colors.white,
+});
+
+globalStyle(`html.${darkMode} *::-webkit-scrollbar-thumb`, {
+  backgroundColor: colors.coolGray[700],
+  boxShadow: `0 0 0 2px inset ${colors.coolGray[900]}`,
+});
+
+globalStyle("*::-webkit-scrollbar-thumb", {
+  backgroundColor: colors.coolGray[300],
+  boxShadow: "0 0 0 2px inset white",
+  borderRadius: 6,
+});
+
+globalStyle("h1", {
+  fontSize: "32px",
+  fontWeight: 600,
+  margin: 0,
+});
+
+globalStyle("h2", {
+  fontSize: "24px",
+  fontWeight: 600,
+  margin: 0,
+});
+
+globalStyle("h3", {
+  fontSize: "20px",
+  fontWeight: 600,
+  margin: 0,
+});
+
+globalStyle("h4", {
+  fontSize: "16px",
+  fontWeight: 600,
+  margin: 0,
+});
--- packages/ds/src/theme.css.ts
+++ packages/ds/src/theme.css.ts
@@ -1,136 +1,21 @@
-import { globalStyle } from "@vanilla-extract/css";
 import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
 
-import colors from "./colors";
 import { vars } from "./vars.css";
 
 export const darkMode = "dark";
 
-globalStyle(`.${darkMode}`, {
-  background: colors.coolGray[900],
-  color: colors.coolGray[400],
-});
-
-globalStyle("html", {
-  fontFeatureSettings: "'ss01' on,'ss02' on,'cv01' on,'cv03' on",
-  background: colors.white,
-  color: colors.coolGray[600],
-});
-
-globalStyle("::selection", {
-  background: colors.blue[500],
-  color: colors.black,
-});
-
-globalStyle(`${darkMode} ::placeholder`, {
-  color: colors.coolGray[500],
-});
-
-globalStyle("::placeholder", {
-  color: colors.coolGray[400],
-  opacity: 1,
-});
-
-// NOTE: -webkit-* for some reason doesn't work with just .dark, requires element.dark.
-globalStyle(`html.${darkMode} *::-webkit-scrollbar`, {
-  background: colors.coolGray[900],
-});
-
-globalStyle("*::-webkit-scrollbar", {
-  background: colors.white,
-  width: 12,
-  height: 12,
-});
-
-globalStyle(`html.${darkMode} *::-webkit-scrollbar-corner`, {
-  background: colors.coolGray[900],
-});
-
-globalStyle("*::-webkit-scrollbar-corner", {
-  background: colors.white,
-});
-
-globalStyle(`html.${darkMode} *::-webkit-scrollbar-thumb`, {
-  backgroundColor: colors.coolGray[700],
-  boxShadow: `0 0 0 2px inset ${colors.coolGray[900]}`,
-});
-
-globalStyle("*::-webkit-scrollbar-thumb", {
-  backgroundColor: colors.coolGray[300],
-  boxShadow: "0 0 0 2px inset white",
-  borderRadius: 6,
-});
-
-globalStyle("h1", {
-  fontSize: "32px",
-  fontWeight: 600,
-  margin: 0,
-});
-
-globalStyle("h2", {
-  fontSize: "24px",
-  fontWeight: 600,
-  margin: 0,
-});
-
-globalStyle("h3", {
-  fontSize: "20px",
-  fontWeight: 600,
-  margin: 0,
-});
-
-globalStyle("h4", {
-  fontSize: "16px",
-  fontWeight: 600,
-  margin: 0,
-});
-
-const responsiveStyles = defineProperties({
+const responsiveProperties = defineProperties({
   conditions: {
-    lightMode: {},
-    darkMode: { selector: `.${darkMode} &` },
+    mobile: {},
+    tablet: { "@media": "screen and (min-width: 768px)" },
+    desktop: { "@media": "screen and (min-width: 1000px)" },
   },
-  defaultCondition: "lightMode",
+  defaultCondition: "mobile",
   properties: {
-    color: vars.color,
-    background: vars.color,
-    border: vars.border,
-    borderLeft: vars.border,
-    borderTop: vars.border,
-    borderBottom: vars.border,
-    borderRight: vars.border,
-    outline: vars.outline,
-    cursor: ["default", "pointer"],
-    opacity: [0.5, 1],
-    display: [
-      "none",
-      "block",
-      "flex",
-      "grid",
-      "inline-block",
-      "inline",
-      "inline-flex",
-    ],
-    position: ["fixed", "absolute", "relative", "sticky"],
-    overflowY: ["auto", "hidden", "scroll"],
-    zIndex: [1, 10, 100, 1000, 10000, 100000],
     top: vars.space,
     bottom: vars.space,
     left: vars.space,
     right: vars.space,
-    visibility: ["hidden", "visible"],
-    flexDirection: ["row", "column"],
-    flex: [1],
-    alignItems: ["stretch", "flex-start", "center", "flex-end"],
-    justifyContent: [
-      "stretch",
-      "flex-start",
-      "center",
-      "flex-end",
-      "space-between",
-    ],
-    listStyle: ["none"],
-    overflow: ["auto", "hidden"],
     gap: vars.space,
     paddingTop: vars.space,
     paddingBottom: vars.space,
@@ -147,30 +32,88 @@ const responsiveStyles = defineProperties({
     height: vars.size,
     minHeight: vars.size,
     maxHeight: vars.size,
-    borderRadius: vars.borderRadius,
-    fontFamily: vars.fontFamily,
-    appearance: ["none"],
-    fontSize: vars.fontSize,
-    fontWeight: vars.fontWeight,
-    fontFeatureSettings: vars.fontFeatureSettings,
-    lineHeight: [1, 1.5, 1.75, 2],
-    textAlign: ["center", "left", "right"],
-    boxShadow: vars.boxShadow,
-    borderCollapse: ["separate", "collapse"],
-    borderSpacing: [0],
-    fill: vars.color,
-    stroke: vars.color,
-    WebkitAppearance: vars.WebkitAppearance,
-    textDecoration: ["none", "underline"],
-    textShadow: vars.textShadow,
   },
   shorthands: {
     padding: ["paddingTop", "paddingBottom", "paddingLeft", "paddingRight"],
     paddingX: ["paddingLeft", "paddingRight"],
     paddingY: ["paddingTop", "paddingBottom"],
-    placeItems: ["alignItems", "justifyContent"],
-    typeSize: ["fontSize", "lineHeight"],
   },
 });
 
-export const atoms = createSprinkles(responsiveStyles);
+const colorProperties = defineProperties({
+  conditions: {
+    lightMode: {},
+    darkMode: { selector: `.${darkMode} &` },
+  },
+  defaultCondition: "lightMode",
+  properties: {
+    color: vars.color,
+    outline: vars.outline,
+    background: vars.color,
+    border: vars.border,
+    borderLeft: vars.border,
+    borderTop: vars.border,
+    borderBottom: vars.border,
+    borderRight: vars.border,
+    boxShadow: vars.boxShadow,
+    textShadow: vars.textShadow,
+    fill: vars.color,
+    stroke: vars.color,
+  },
+});
+
+const fontProperties = defineProperties({
+  properties: {
+    textDecoration: ["none", "underline"],
+    textAlign: ["center", "left", "right"],
+    fontFamily: vars.fontFamily,
+    fontSize: vars.fontSize,
+    fontWeight: vars.fontWeight,
+    fontFeatureSettings: vars.fontFeatureSettings,
+    lineHeight: [1, 1.5, 1.75, 2],
+  },
+});
+
+const otherProperties = defineProperties({
+  properties: {
+    borderRadius: vars.borderRadius,
+    cursor: ["default", "pointer"],
+    opacity: [0.5, 1],
+    position: ["fixed", "absolute", "relative", "sticky"],
+    overflowY: ["auto", "hidden", "scroll"],
+    zIndex: [1, 10, 100, 1000, 10000, 100000],
+    visibility: ["hidden", "visible"],
+    flexDirection: ["row", "column"],
+    flex: [1],
+    alignItems: ["stretch", "flex-start", "center", "flex-end"],
+    justifyContent: [
+      "stretch",
+      "flex-start",
+      "center",
+      "flex-end",
+      "space-between",
+    ],
+    listStyle: ["none"],
+    overflow: ["auto", "hidden"],
+    display: [
+      "none",
+      "block",
+      "flex",
+      "grid",
+      "inline-block",
+      "inline",
+      "inline-flex",
+    ],
+    WebkitAppearance: vars.WebkitAppearance,
+    borderCollapse: ["separate", "collapse"],
+    borderSpacing: [0],
+    appearance: ["none"],
+  },
+});
+
+export const atoms = createSprinkles(
+  responsiveProperties,
+  colorProperties,
+  fontProperties,
+  otherProperties
+);
