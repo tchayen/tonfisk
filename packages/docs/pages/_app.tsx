135577918722532845f35c2b3ef93a7ff216371a
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-09-20 18:05:29 +0200

    Fix package dependencies

--- packages/docs/pages/_app.tsx
+++ packages/docs/pages/_app.tsx
@@ -5,12 +5,24 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { TonfiskProvider } from "tonfisk";
 
+const title = "tonfisk 🐟";
+const description =
+  "A React design system with full accessible component library.";
+const url = "https://tonfisk.fish";
+const imageUrl = "";
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Head>
-        <title>tonfisk 🐟</title>
+        <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
       </Head>
       <TonfiskProvider>
         <Component {...pageProps} />
