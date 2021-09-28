dc223369435b7388ad744981d3608c8b5114f197
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-09-28 16:19:23 +0200

    Change bundling config

---
+++ packages/tonfisk/src/index.ts
@@ -0,0 +1,35 @@
+export { Button, ButtonLink } from "./components/Button";
+export { Checkbox } from "./components/Checkbox";
+export { TextInput } from "./components/TextInput";
+export { ModalDialog } from "./components/ModalDialog";
+export { TonfiskProvider } from "./Provider";
+export { Select, Item } from "./components/Select";
+export { Switch } from "./components/Switch";
+export { HorizontalLine } from "./components/HorizontalLine";
+export { Tag } from "./components/Tag";
+export { Tooltip } from "./components/Tooltip";
+export { MenuButton } from "./components/Menu";
+export { Pagination } from "./components/Pagination";
+export { Spinner } from "./components/Spinner";
+export { Label } from "./components/Label";
+export { Pill } from "./components/Pill";
+export { Popover } from "./components/Popover";
+export * as commonStyles from "./commonStyles.css";
+export type { Selection } from "./components/Table";
+export {
+  TableRowGroup,
+  TableHeaderRow,
+  TableColumnHeader,
+  TableRow,
+  TableCell,
+  Table,
+  Cell,
+  Column,
+  Row,
+  TableBody,
+  TableHeader,
+  TableSelectAllCell,
+  TableCheckboxCell,
+} from "./components/Table";
+export { Breadcrumbs, BreadcrumbItem } from "./components/Breadcrumbs";
+export { atoms } from "./theme.css";
