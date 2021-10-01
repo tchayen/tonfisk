755020b6132d6d079d7a70b38adee237e3f174e1
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-10-01 19:01:53 +0200

    Add descriptions

--- packages/tonfisk/src/components/Accordion.tsx
+++ packages/tonfisk/src/components/Accordion.tsx
@@ -2,9 +2,21 @@ import { AnimatePresence, motion } from "framer-motion";
 import React, { ReactNode, useState } from "react";
 
 type Props = {
+  /**
+   * If set, accordion will start open.
+   */
   mountOpen?: boolean;
+  /**
+   * JSX for the component to be displayed as header. Enclosed in `<header>` tag.
+   */
   header: ReactNode;
+  /**
+   * Accordion content that can be minimized and expanded. Enclosed in `<section>` tag.
+   */
   children: ReactNode;
+  /**
+   * Styling of the header applied to the `<header>`.
+   */
   className?: string;
 };
 
