d2b0668eb5b3f474e2b6f542767f7fa43f50dd13
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-10-02 05:42:58 +0200

    Improve accessibility of HorizontalLine and Spinner

---
+++ packages/tonfisk/src/components/HorizontalLine.css.ts
@@ -0,0 +1,10 @@
+import { atoms } from "../theme.css";
+
+export const horizontalLine = atoms({
+  background: {
+    lightMode: "gray-200",
+    darkMode: "gray-700",
+  },
+  width: "100%",
+  height: "1px",
+});
